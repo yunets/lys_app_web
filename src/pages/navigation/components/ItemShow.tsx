6bf9977223507c82f8870c613bcdc2c7edc31cc2
liuyunshengsir <724547564@qq.com>
2023-02-22 15:29:14 +0800

    样式调整

--- src/pages/navigation/components/ItemShow.tsx
+++ src/pages/navigation/components/ItemShow.tsx
@@ -19,14 +19,17 @@ const ItemShow: React.FC<Props> = () => {
       <div>
         <div className={styles.urlContent}>
           <a>
-            <img src="https://cdn.jsdelivr.net/gh/volfclub/Tundra-Wolf/assets/logos/o2ov.png" />
+            <img
+              className={styles.urlImg}
+              src="https://cdn.jsdelivr.net/gh/volfclub/Tundra-Wolf/assets/logos/o2ov.png"
+            />
           </a>
           <div>
             <a href="###">
-              <strong>Bara Art</strong>
+              <strong>MongoDB</strong>
             </a>
 
-            <p>免费照片图库</p>
+            <p>自定义的网站导航，自定义的网站导航，导航</p>
           </div>
         </div>
       </div>
