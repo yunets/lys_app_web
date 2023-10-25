b5eeee1e17553628129fbeb687bbc5d3e80ef0de
15063036525 <578888218@qq.com>
2023-10-25 21:02:46 +0800

    update

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -12,7 +12,7 @@ const Settings: LayoutSettings & {
   fixedHeader: true,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: '公益chatgpt导航',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
