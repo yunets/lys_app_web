c82b7f7d836d6e0ae2a2e8542bb19e804ccb64f2
15063036525 <578888218@qq.com>
2023-12-16 18:03:55 +0800

    update

--- src/pages/chatgpt/MyChat5.tsx
+++ src/pages/chatgpt/MyChat5.tsx
@@ -1,9 +1,10 @@
+import { formatDate } from '@/utils/TimeUtils';
 import { GridContent, PageContainer } from '@ant-design/pro-layout';
-import { Card, Tabs, Tag } from 'antd';
+import { Card, Form, Tag, Button, Input, Avatar } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 
-
-
+import styles from './index.less';
+import gpt from '@/assets/img/gpt.png';
 export interface Props {
     name: string;
 }
@@ -20,7 +21,7 @@ const MyChat5: React.FC<Props> = () => {
 
 
     useEffect(() => {
-        ws.current = new WebSocket("ws://localhost/appchat/" + localStorage.getItem("user-ip"));
+        ws.current = new WebSocket("ws://124.220.104.235/appchat/" + localStorage.getItem("user-ip"));
         ws.current.onopen = function () {
             // Web Socket 已连接上，使用 send() 方法发送数据
             const a = {
@@ -42,6 +43,7 @@ const MyChat5: React.FC<Props> = () => {
             if (typeof obj == 'object' && obj) {
                 // list.push(obj);
                 // setList([...list, obj]);
+
                 setList((prevState) => [...prevState, obj]);
             }
         };
@@ -54,18 +56,56 @@ const MyChat5: React.FC<Props> = () => {
         };
 
     }, []);
+    const [form] = Form.useForm();
+
+    const handleSearch = async () => {
+        form.validateFields()
+            .then((values) => {
+
+                const a = {
+                    "ip": localStorage.getItem("user-ip"),
+                    "address": localStorage.getItem("user-address"),
+                    "msg": values.name,
+                    "time": formatDate(new Date())
+                };
+                ws.current.send(JSON.stringify(a));
 
 
+
+            })
+            .catch((errorInfo) => {
+                console.log(errorInfo);
+            });
+    };
     return (
         <PageContainer>
 
 
 
             <GridContent>
-                <h3><Tag color="magenta">！{JSON.stringify(list)}</Tag></h3>
-                {list.map((item: any) => {
-                    return <Tag color="magenta" key={item.ip}>{item.msg}</Tag>;
-                })}
+                <h3><Tag color="magenta">chatgpt在线交流</Tag></h3>
+                <div className={styles.myChatLeft}>
+                    {list.map((item: any) => {
+                        return <Tag color="#108ee9" key={item.ip} >
+                            <Avatar src={<img src={gpt} alt="avatar" />} />
+                            {item.address} {item.time}
+                            <br />
+                            {JSON.stringify(item)}</Tag>;
+                    })}
+                </div>
+                <Form form={form} className={styles.myChatForm}>
+
+                    <Form.Item
+                        name="name"
+                        label="名称"
+                        rules={[{ message: '请输入内容', whitespace: true }]}
+                    >
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item> <Button type="primary" onClick={() => { handleSearch(); }}>
+                        群发消息
+                    </Button></Form.Item>
+                </Form>
 
             </GridContent>
 
