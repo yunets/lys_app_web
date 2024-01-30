0a26b6315cf35bd14b22737a762887cfe22bc847
15063036525 <578888218@qq.com>
2024-01-30 09:48:07 +0800

    update

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -9,3 +9,26 @@ export function formatDate(date: Date): string {
 
 
 }
+
+
+export function getDaysDifference(date1: string, date2: string) {
+
+    // 将字符串转换为Date对象  
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+
+    // 确保日期字符串格式正确，例如 "YYYY-MM-DD"  
+    if (isNaN(d1.getTime()) || isNaN(d2.getTime())) {
+        return false;
+    }
+
+    // 计算两个日期之间的差异（以毫秒为单位）  
+    const diff = Math.abs(d2.getTime() - d1.getTime());
+
+    // 天 = 7 * 24 * 60 * 60 * 1000 毫秒  
+    const daysInMilliseconds = 24 * 60 * 60 * 1000;
+
+    // 检查差异是否小于或等于7天（以毫秒为单位）  
+    return diff / daysInMilliseconds;
+}
+
