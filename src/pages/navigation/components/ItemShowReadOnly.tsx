def34a4416d5ceb89fff4359a562efbfa043c834
578888218 <578888218@qq.com>
2024-03-08 09:50:10 +0000

    收藏

--- src/pages/navigation/components/ItemShowReadOnly.tsx
+++ src/pages/navigation/components/ItemShowReadOnly.tsx
@@ -2,82 +2,80 @@ import { Fragment } from 'react';
 import React from 'react';
 import styles from './../index.less';
 import { connect } from 'umi';
-
+import shoucangyes from '@/assets/svg/shoucangyes.svg';
+import shoucangno from '@/assets/svg/shoucangno.svg';
+import dashang from '@/assets/svg/dashang.svg';
 
 export interface Props {
   item: any;
 }
 
 const ItemShowReadOnly: React.FC<Props> = ({ item }) => {
-
-
-
-
-
-  const getImg = (url: { url: string | URL; }) => {
+  const getImg = (url: { url: string | URL }) => {
     const u = new URL(url.url);
     // return "" + u.origin + "/favicon.ico";
     // return "https://api.iowen.cn/favicon/www.aliued.cn.png";
-    return "https://api.iowen.cn/favicon/" + u.origin.replace("https://", "").replace("http://", "") + ".png";
-  }
-
-
-
-
-
-
-
-
-
+    return (
+      'https://api.iowen.cn/favicon/' +
+      u.origin.replace('https://', '').replace('http://', '') +
+      '.png'
+    );
+  };
 
   return (
     <Fragment>
-      <div>
+      <div className={styles.urlCard}>
+        <div className={styles.urlOperate}>
+          <img src={shoucangyes} className={styles.toolImg} />
+          <img src={shoucangno} className={styles.toolImg} />
+          <img src={dashang} className={styles.toolImg} />
 
-
-        {/* <div className={styles.urlOperate}>
-          <DeleteOutlined onClick={() => fetchUpdateWebInfoList(item)} spin title='点击删除！！！' />
+          {/* <DeleteOutlined onClick={() => fetchUpdateWebInfoList(item)} spin title='点击删除！！！' />
           <HeartTwoTone twoToneColor="#eb2f96" onClick={() => fetchUpdateWebInfoList(item)} title='收藏！！！' />
-          <HomeTwoTone twoToneColor="#222222" onClick={() => fetchUpdateWebInfoList(item)} title='添加到首页！！！' />
-        </div> */}
+          <HomeTwoTone twoToneColor="#222222" onClick={() => fetchUpdateWebInfoList(item)} title='添加到首页！！！' /> */}
+        </div>
 
         <div className={styles.urlContent}>
-
-          <a onClick={() => { console.log("添加到个人收藏") }}>
-
+          <a
+            onClick={() => {
+              console.log('添加到个人收藏');
+            }}
+          >
             <img
               className={styles.urlImg}
-              src={getImg(item)} onError={(e) => {
+              src={getImg(item)}
+              onError={(e) => {
                 const img = e.currentTarget;
-                img.src = "https://api.iowen.cn/favicon/www.aliued.cn.png";
+                img.src = 'https://api.iowen.cn/favicon/www.aliued.cn.png';
                 (img as any).οnerrοr = null;
                 // img.οnerrοr = null;
-
-
               }}
             />
           </a>
           <div className={styles.urlText}>
-            <a onClick={() => {
-              window.open("" + item.url + "", '_blank',);
-            }} rel="noreferrer">
+            <a
+              onClick={() => {
+                window.open('' + item.url + '', '_blank');
+              }}
+              rel="noreferrer"
+            >
               <strong>{item.name}</strong>
             </a>
             <p>{item.comment}</p>
           </div>
         </div>
       </div>
-    </Fragment >
+    </Fragment>
   );
 };
 
-
-function mapStateToProps(state: any) { //state是项目所有的models
+function mapStateToProps(state: any) {
+  //state是项目所有的models
   const { list } = state.navigation; //获取namespace命名空间为navigation的models数据state
   const { title } = state.navigation;
   return {
     cards: list.content,
-    title
+    title,
   };
 }
 export default connect(mapStateToProps)(ItemShowReadOnly as any);
